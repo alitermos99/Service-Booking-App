c6ebc589d6ecf3838abce498c9b08557872f2c10
Ali Termos <ali1999.termos@hotmail.com>
2026-06-02 16:58:52 +0300

    build(v1): remove margin and max widht from Header, and fix the background of the avatar not to change on every click UserAvatar

--- client/app/components/ui/UserAvatar.jsx
+++ client/app/components/ui/UserAvatar.jsx
@@ -15,6 +15,7 @@ const getRandomLinearGradient = () => {
 
 const UserAvatar = ({ user, children, showDropdown = false }) => {
     const [active, setActive] = useState(false);
+    const [gradient] = useState(getRandomLinearGradient);
     const initials = user ? user.name?.split(' ').map(n => n[0]).join('').toUpperCase() : 'AN';
 
     const handleDropdown = () => {
@@ -25,7 +26,7 @@ const UserAvatar = ({ user, children, showDropdown = false }) => {
         <div className="relative cursor-pointer">
             <div className="flex items-center gap-2" onClick={handleDropdown}>
                 <div className="w-8 h-8 rounded-full flex items-center justify-center text-white text-xs font-bold"
-                    style={{ background: getRandomLinearGradient() }}
+                    style={{ background: gradient }}
                 >
                     { initials }
                 </div>
