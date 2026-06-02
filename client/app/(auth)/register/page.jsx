9f34fac0c5bf8c5a2440efdd8b9cb77b7f75e4c3
Ali Termos <ali1999.termos@hotmail.com>
2026-06-02 14:16:49 +0300

    build(v1): orevent user from registering when already logged in in RegisterPage

--- client/app/(auth)/register/page.jsx
+++ client/app/(auth)/register/page.jsx
@@ -1,14 +1,25 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import Main from '@/app/components/layouts/Main'
 import AuthHeader from '@/app/components/auth/AuthHeader';
 import Card from '@/app/components/ui/Card';
 import RegisterForm from '@/app/components/forms/RegisterForm';
 import AuthFooter from '@/app/components/auth/AuthFooter';
 import Orb from '@/app/components/ui/Orb';
+import { useRouter } from 'next/navigation';
+import { useUser } from '@/app/features/auth/hooks/useUser';
 
 const RegisterPage = () => {
+    const router = useRouter();
+    const { data: user, isPending } = useUser();
+
+    useEffect(() => {
+        if (user) router.push("/");
+    }, [user, router]);
+
+    if (isPending) return null;
+
     return (
         <Main className={'animate-fade-up'}>
             <Orb size="w-96 h-96" opacity={0.20} className="top-0 right-0 translate-x-1/3 -translate-y-1/3 text-accent-soft" />
