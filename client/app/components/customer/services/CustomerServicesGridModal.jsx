5303c6f95be0fd58195aa0c91a567268617557ac
Ali Termos <ali1999.termos@hotmail.com>
2026-06-09 17:13:49 +0300

    build(v1): create component CustomerServicesGridModal

---
+++ client/app/components/customer/services/CustomerServicesGridModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerServicesGridModal = () => {
+    return (
+        <div>CustomerServicesGridModal</div>
+    )
+}
+
+export default CustomerServicesGridModal
