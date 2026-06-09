75bc4cae51689a127073f4a025ef98458af72d5c
Ali Termos <ali1999.termos@hotmail.com>
2026-06-09 14:10:18 +0300

    build(v1): rename CustomerServicesCategoryShips to filtersection

--- client/app/components/customer/services/CustomerServicesCategoryShips.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CustomerServicesCategoryShips = () => {
-    return (
-        <div>CustomerServicesCategoryShips</div>
-    )
-}
-
-export default CustomerServicesCategoryShips
---
+++ client/app/components/customer/services/CustomerServicesFilterSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerServicesFilterSection = () => {
+    return (
+        <div>CustomerServicesFilterSection</div>
+    )
+}
+
+export default CustomerServicesFilterSection
