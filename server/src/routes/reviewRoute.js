6871e188c8fa6df9cfb52ad015efd9305c7875b8
Ali Termos <ali1999.termos@hotmail.com>
2026-06-11 18:09:44 +0300

    build(v1): added authorize customers to only create reviews reviewRoute

--- server/src/routes/reviewRoute.js
+++ server/src/routes/reviewRoute.js
@@ -5,10 +5,12 @@ import {
 	deleteReview
 } from '../controllers/reviewController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
+import { authorize } from '../middlewares/roleMiddleware.js';
 
 const router = express.Router();
 
 router.use(authMiddleware);
+router.use(authorize('customer'));
 
 router.post('/', createReview);
 router.put('/:id', updateReview);
