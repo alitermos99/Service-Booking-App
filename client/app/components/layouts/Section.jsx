a8fad0d435fa00cc10db8cde53fcf71ad177e6d4
Ali Termos <ali1999.termos@hotmail.com>
2026-06-16 18:14:45 +0300

    build(v1): added the sm:px-6 to the attr sent instead of fixed in the html Section component

--- client/app/components/layouts/Section.jsx
+++ client/app/components/layouts/Section.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Section = ({ id, children, padding = 'py-24 px-4', className = '', maxWidth = 'max-w-7xl', style }) => {
+const Section = ({ id, children, padding = 'py-24 px-4 sm:px-6', className = '', maxWidth = 'max-w-7xl', style }) => {
     return (
         <section
             id={id}
             style={style}
-            className={`${padding} sm:px-6 ${maxWidth} mx-auto ${className}`}
+            className={`${padding} ${maxWidth} mx-auto ${className}`}
         >
             {children}
         </section>
