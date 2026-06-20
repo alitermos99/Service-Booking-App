003048ee4ee7385219d74e14b1cd6b3185bcb599
Ali Termos <ali1999.termos@hotmail.com>
2026-06-20 15:17:59 +0300

    build(v1): populate adminid name in serviceUtils getServiceByIdOrThrow

--- server/src/utils/serviceUtils.js
+++ server/src/utils/serviceUtils.js
@@ -2,7 +2,7 @@ import Service from '../models/Service.js';
 import ApiError from "../errors/ApiError.js";
 
 export const getServiceByIdOrThrow = async (serviceId) => {
-	const service = await Service.findById(serviceId);
+	const service = await Service.findById(serviceId).populate("admin_id", "name");
 
 	if (!service) {
 		throw new ApiError("Service not found", 404);
