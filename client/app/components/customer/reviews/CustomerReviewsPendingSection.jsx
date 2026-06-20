f5c12ac2f531b42d3cf263aaf0860a0cb339080c
Ali Termos <ali1999.termos@hotmail.com>
2026-06-20 19:15:18 +0300

    build(v1): create component CustomerReviewsPendingSection

---
+++ client/app/components/customer/reviews/CustomerReviewsPendingSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerReviewsPendingSection = () => {
+    return (
+        <div>CustomerReviewsPendingSection</div>
+    )
+}
+
+export default CustomerReviewsPendingSection
