9f39f124a4fdbcc2d88afa759ea53aa781a147c5
Ali Termos <ali1999.termos@hotmail.com>
2026-06-20 19:19:57 +0300

    build(v1): create CustomerReviewsPendingCard component

---
+++ client/app/components/customer/reviews/CustomerReviewsPendingCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerReviewsPendingCard = () => {
+    return (
+        <div>CustomerReviewsPendingCard</div>
+    )
+}
+
+export default CustomerReviewsPendingCard
